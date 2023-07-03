d185575f12c20a7b6fde749c577c282717d1ab4c
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-07-03 16:34:18 +0200

    about started

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Myself</h1>
+  </div>
+  <div class="container-fluid">
+    <img src="https://i.postimg.cc/1ts3jCbs/C12-Cheslyn-Herman-1.jpg" alt="Cheslyn Herman" class="aboutImg img-fluid">
+  </div>
+  <div class="container-fluid">
+    <h3>Bio</h3>
+    <p>I am an aspiring web developer. I am someone who is disciplined, hardworking and goal driven.
+My willingness to learn allows me to constantly seek new challenges and I can adapt to any environment. 
+I work efficiently and effectively both within a team as well as individually.</p>
   </div>
 </template>
