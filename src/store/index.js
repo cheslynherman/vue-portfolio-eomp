9838e826cb233884a3ee9566c91b368db8bc5192
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-07-05 16:31:50 +0200

    projects & testimonials

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    projects: null,
+    testimonials: null,
   },
   mutations: {
+    setProjects: (state, projects) => {
+      state.projects = projects
+    },
+    setTestimonials: (state, testimonials) => {
+      state.testimonials = testimonials
+    },
   },
   actions: {
+    getProjects: async (context) => {
+      fetch ("https://cheslynherman.github.io/data/projects.json")
+      .then ((res) => res.json())
+      .then ((projects) => context.commit("setProjects", projects))
+    },
+    getTestimonials: async (context) => {
+      fetch ("https://cheslynherman.github.io/data/testimonials.json")
+      .then ((res) => res.json())
+      .then ((testimonials) => context.commit("setTestimonials", testimonials))
+    },
   },
-  modules: {
-  }
 })
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,3 +1,52 @@
 <template>
-    
+  <h3>Projects</h3>
+  <div class="main">
+    <div v-if="projects" class="container fluid">
+      <ProjectCardComp
+        v-for="project of projects"
+        :key="project.id"
+        :project="project"
+      />
+    </div>
+    <div v-else>Sorry bro</div>
+  </div>
+
+  <h3>Testimonials</h3>
+  <div>
+    <div v-if="testimonials" class="container fluid">
+      <TestimonialsCardComp
+        v-for="testimonial of testimonials"
+        :key="testimonial.id"
+        :testimonial="testimonial"
+      />
+    </div>
+    <div v-else>Sorry bro</div>
+  </div>
 </template>
+
+<script>
+import ProjectCardComp from "@/components/ProjectCard-comp.vue";
+import TestimonialsCardComp from "@/components/TestimonialsCard-comp.vue";
+export default {
+  computed: {
+    projects() {
+      return this.$store.state.projects;
+    },
+    testimonials () {
+        return this.$store.state.testimonials;
+    }
+  },
+  mounted() {
+    this.$store.dispatch("getProjects"),
+    this.$store.dispatch("getTestimonials");
+  },
+  components: { ProjectCardComp, TestimonialsCardComp },
+};
+</script>
+
+<style scoped>
+.main {
+   display: grid !important;
+   grid-template-columns: auto auto auto;
+}
+</style>
